41d3f8a79cf206f5b600b42a22d23a5d73e7247f
Sahil Shrestha <sahiltuchhe123@gmail.com>
2024-09-17 13:07:44 +0545

    Dom manupulation

---
+++ src/34_Dom.js
@@ -0,0 +1,77 @@
+// JavaScript DOM
+
+/*
+// Selecting  Element
+document.getElementById("elementId");
+document.getElementByClassName("className")
+document.getElementByTagName("tagName")
+document.querySelector("cssSelector")
+document.querySelectorAll("cssSelector")
+
+
+// Travesing the DOM
+element.parentNode;
+element.childNode;
+element.firstChild;
+element.lastChild;
+element.previousSibling;
+element.nextSibling;
+
+
+// Modifying Elements
+element.innerHtml;
+element.textContent;
+element.getArrtibute("attributeName", "value")
+element.getArrtibute("attributeName")
+
+
+//Creating and Appending Elements
+
+document.createElement("tagName");
+document.createTextNode("text content");
+parentElement.appendChaild(chaildElement);
+parentElement.insertBefore(newElement,referenceElement);
+parentElement.removeChild(childElement)
+
+
+//Event Handling
+element.addEventListner("event",function)
+element.removeEventListner("event",function)
+
+
+// Modifying Classes
+element.classList.add("className")
+element.classList.("className")
+element.classList.("className")
+
+
+// Styles
+element.styles.property="value";
+
+// Manupulating Attributes
+element.setAttribute("attributeName","value")
+element.getAttribute("attributeName")
+element.removeAttribute("attributeName")
+
+
+//Modifying Content
+element.innerHTML ="New content"
+element.texttContent ="New text content"
+
+
+// Manupulating Classes
+element.classList.add("className")
+element.classList.remove("className")
+element.classList.toggle("className")
+
+
+// Event Handling
+element.addEventListner("click",function)
+element.removeEventListner("click",function)
+
+
+// Window Methods
+window.alert("Message")
+window.prompt("Prompt message", "default value")
+window.confirm("Confirmation message");
+*/
---
+++ src/36_domlearning.js
@@ -0,0 +1,21 @@
+// console.log(document.getElementById('title').getAttribute("id"))
+// console.log(document.getElementById('title').getAttribute("class"))
+// console.log(document.getElementById('title').getAttribute("class", "test heading"));
+
+
+// title.style.backgroundColor = "green"
+// title.style.padding = "15px"
+// title.style.borderRaduis = "15px"
+
+
+
+// console.log(title.innerHTML)// All are visible and also html tags are visible
+// console.log(title.textContent)// Hidden content are not visible
+// console.log(title.innerText)// All content is visible 
+
+
+console.log(document.getElementsByClassName("heading"))
+
+// console.log(document.querySelector("h1"))
+console.log(document.querySelector("h2"))
+console.log(document.querySelector("#title"))

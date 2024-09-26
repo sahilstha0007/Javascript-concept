a0c3dd699eb5953db7dd280d8579e9d880ab6a7f
Sahil Shrestha <sahiltuchhe123@gmail.com>
2024-09-26 14:08:50 +0545

    Remmove images

--- src/40_Events.js
+++ src/40_Events.js
@@ -14,9 +14,9 @@
 
 
 // Third parameter false default in addEventListener
-document.getElementById("images").addEventListener("click", (event) => {
-    console.log("clicked inside the ul")
-}, false)
+// document.getElementById("images").addEventListener("click", (event) => {
+//     console.log("clicked inside the ul")
+// }, false)
 
 // Event propogation
 // diff of false and true in third parameter
@@ -25,11 +25,41 @@ document.getElementById("images").addEventListener("click", (event) => {
 
 // Examples
 // Bubbling
-document.getElementById("owl").addEventListener("click", (event) => {
-    console.log("owl clicked")
+// document.getElementById("owl").addEventListener("click", (event) => {
+//     console.log("owl clicked")
+// }, false)
+// //Capturing
+// document.getElementById("owl").addEventListener("click", (event) => {
+//     console.log("owl clicked")
+//     event.stopPropagation
+// }, true)
+
+
+
+// document.getElementById("images").addEventListener("click", (event) => {
+//     console.log("clicked inside the ul")
+// }, false)
+
+// document.getElementById("owl").addEventListener("click", (event) => {
+//     console.log("owl clicked")
+//     event.stopPropagation()
+// }, false)
+
+// document.getElementById('google').addEventListener("click", (event) => {
+//     event.preventDefault()
+//     event.stopPropagation()
+//     console.log("Google clicked");
+// }, false)
+
+
+document.querySelector("#images").addEventListener("click", (event) => {
+    console.log(event.target.tagName);
+    if (event.target.tagName ==="IMG"){
+        console.log(event.target.id);
+        let removeIt = event.target.parentNode
+        // removeIt.parentNode.removeChild(removeIt)
+        removeIt.remove()
+        
+    }
+    // removeIt.remove()
 }, false)
-//Capturing
-document.getElementById("owl").addEventListener("click", (event) => {
-    console.log("owl clicked")
-    event.stopPropagation
-}, true)
